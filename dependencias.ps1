68a7df2765af7735eeff733f3d40d7e2155baf13
rodrigo <royopa@gmail.com>
2025-02-12 18:22:24 -0300

    ajuste workflow github actions

--- dependencias.ps1
+++ dependencias.ps1
@@ -1,6 +1,7 @@
 #Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+Set-PSRepository PSGallery -InstallationPolicy Trusted
 #Install-Module PowerShellGet -AllowClobber -Force -Scope CurrentUser -SkipPublisherCheck
 #Install-Module -Name PSScriptAnalyzer -Force -Scope CurrentUser -AllowClobber
-Install-Module -Name FXPSYaml -Scope CurrentUser -Yes
-Install-Module -Name SqlServer -Scope CurrentUser -Force -Yes
-Install-Module -Name PsSqlClient -AllowPrerelease -Scope CurrentUser -Force -AllowClobber -Yes
+Install-Module -Name FXPSYaml -Scope CurrentUser -Force
+Install-Module -Name SqlServer -Scope CurrentUser -Force
+Install-Module -Name PsSqlClient -AllowPrerelease -Scope CurrentUser -Force -AllowClobber
