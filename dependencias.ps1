106804811f1ccc51225da6141353086974b2ff58
rodrigo <royopa@gmail.com>
2025-02-12 16:15:46 -0300

    adiciona gitignore para projeto powershell python e dotnetcore

--- dependencias.ps1
+++ dependencias.ps1
@@ -2,4 +2,5 @@
 #Install-Module PowerShellGet -AllowClobber -Force -Scope CurrentUser -SkipPublisherCheck
 Install-Module -Name FXPSYaml -Scope CurrentUser
 Install-Module -Name SqlServer -Scope CurrentUser -Force
-Install-Module -Name PsSqlClient -AllowPrerelease -Scope CurrentUser -Force -AllowClobber 
+Install-Module -Name PsSqlClient -AllowPrerelease -Scope CurrentUser -Force -AllowClobber
+Install-Module -Name PSScriptAnalyzer -Force -Scope CurrentUser -AllowClobber
