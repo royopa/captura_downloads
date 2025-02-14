5cfccc620f58b61f03e47e53c3916284cac53ea0
rodrigo <royopa@gmail.com>
2025-02-14 07:21:09 -0300

    added some dependencies for dotnet project

--- Program.cs
+++ Program.cs
@@ -1,9 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Signed integral types:");
-
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+﻿Console.WriteLine("2: Second");
