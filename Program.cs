013556e24b489f8f950600df8e2849ca0b3c4c95
rodrigo <royopa@gmail.com>
2025-02-14 16:08:18 -0300

    corrige println

--- Program.cs
+++ Program.cs
@@ -1,6 +1,1 @@
-﻿using System.Diagnostics.Debug;
-using.System.Diagnostics.Trace;
-
-Console.WriteLine("This message is readable by the end user.");
-Trace.WriteLine("This is a trace message when tracing the app.");
-Debug.WriteLine("This is a debug message just for developers.");
+﻿Console.WriteLine("Captura downloads");
