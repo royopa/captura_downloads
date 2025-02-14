d318d808cf579da0862e7acf7b8423818cb908bd
rodrigo <royopa@gmail.com>
2025-02-14 15:03:18 -0300

    adiciona selenium python

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("2: Second");
+﻿using System.Diagnostics.Debug;
+using.System.Diagnostics.Trace;
+
+Console.WriteLine("This message is readable by the end user.");
+Trace.WriteLine("This is a trace message when tracing the app.");
+Debug.WriteLine("This is a debug message just for developers.");
---
+++ download_files_selenium.py
@@ -0,0 +1,10 @@
+from splinter import Browser
+
+
+def main():
+    browser = Browser('edge', incognito=True)
+    browser.visit('https://www.google.com')
+
+
+if __name__ == "__main__":
+    main()
